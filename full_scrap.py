98de0db7e0338b2cdcbe84cacfae9dc5c02ce063
Ostend <smennear.nccu@gmail.com>
2021-04-07 13:06:49 +0200

    scraping-loop

--- full_scrap.py
+++ full_scrap.py
@@ -3,31 +3,40 @@ import requests
 import time
 import re
 
-
-def article_url():
-    #took the global from single_article (article_content) to test the bug of empty array
+#Get first page url, then all sequential pages, you can determine how many pages.
+def next_page():
     global article_content
     article_content = []
-    global list_uris
-    list_uris = []
-    global next_url_snippet 
-    next_url_snippet = []
-    article_path = requests.get('https://www.reuters.com/news/archive/worldnews?view=page&page=1&pageSize=10').text
-    soup = BeautifulSoup(article_path, 'lxml')
-    all_url_tags = soup.find_all('div', class_='story-content')
-    for ugly_url in all_url_tags:
-        anchor_url = ugly_url.a
-        clean_urls=anchor_url.get('href')
-        list_uris.append(clean_urls)
-    #call all of our other functions!
-    create_url(list_uris)
-    #^^ call the function that creates a list of full urls
-    single_article(all_urls)
-    #seperate_articles(article_content) #***** placed this in single_article
-    #get the next page. Need to work on this to set it up for infinate loop
-    #next_page()
-    #second_pages(next_url)
+    #************* try to make it so it will not revert back to home page
+    #article_path = requests.get('https://www.reuters.com/world').text
+    counter = 0
+    d = 1
+    global next_url
+    while(counter == 0):
+        more_content_url = 'https://www.reuters.com/news/archive/worldnews?view=page&page={}&pageSize=10'.format(d)
+        article_path = requests.get(more_content_url).text
+        soup = BeautifulSoup(article_path, 'lxml')
+        next_page = soup.find('div', class_="control-nav")
+        anchor_next = next_page.find('a', class_="control-nav-next")
+        next_uri = anchor_next.get('href')
+        counter = 1
+        d +=1
+        next_url = 'https://www.reuters.com/news/archive/worldnews' + next_uri
+        second_pages(next_url)   
+        while (counter >= 1 and counter <= 3):
+            more_content_url = 'https://www.reuters.com/news/archive/worldnews?view=page&page={}&pageSize=10'.format(d)
+            article_path = requests.get(more_content_url).text
+            soup = BeautifulSoup(article_path, 'lxml')
+            next_page = soup.find('div', class_="control-nav")
+            anchor_next = next_page.find('a', class_="control-nav-next")
+            next_uri = anchor_next.get('href')
+           # d += 1
+            next_url = 'https://www.reuters.com/news/archive/worldnews' + next_uri
+            second_pages(next_url)
+            counter += 1
+            d += 1
 
+#find all the links to the individual articles, clean them up (create_url) and put them in list second_list_uri
 def second_pages(link_here):
     global second_list_uris
     second_list_uris = []
@@ -43,10 +52,7 @@ def second_pages(link_here):
     #^^ call the function that creates a list of full urls
     single_article(all_urls)
     seperate_articles(article_content)
-    
-     
-
-       
+                
 #create the url for the individual article    
 def create_url(uri_list):
     global all_urls
@@ -55,8 +61,7 @@ def create_url(uri_list):
         uri = 'https://www.reuters.com/' + uri
         all_urls.append(uri)
 
-
-        
+#go to the individual article to gather the text body, then clean it up by passing it into seperate_articles()
 def single_article(enter_urls):
 
     for url in enter_urls:
@@ -67,11 +72,9 @@ def single_article(enter_urls):
             par = paragraph.text
             article_content.append(par)
     seperate_articles(article_content)
-    #get the next page url of THIS current page!
 
-            
 
-        
+#needs work, but it cleans up the list of <p> tags where the articles are broken up into and combines them, then parses them back into their individual articles into a list. Then writes them to a file.                    
 def seperate_articles(article_body):
     joined_articles = " ".join(article_body)
     parsed_list = joined_articles.split(' (Reuters)')
@@ -79,32 +82,11 @@ def seperate_articles(article_body):
         for item in parsed_list:
             f.write("%s\n" % item)
 
-#Get url for the next page of 10 results
-def next_page():
-    #************* try to make it so it will not revert back to home page
-    #article_path = requests.get('https://www.reuters.com/world').text
 
-    more_content_url = 'https://www.reuters.com/news/archive/worldnews?view=page&page=1&pageSize=10'
-    article_path = requests.get(more_content_url).text
-    soup = BeautifulSoup(article_path, 'lxml')
-    next_page = soup.find('div', class_="control-nav")
-    anchor_next = next_page.find('a', class_="control-nav-next")
-    next_uri = anchor_next.get('href')
-    #create url
-    global next_url
-    next_url = 'https://www.reuters.com/news/archive/worldnews' + next_uri
 
-    second_pages(next_url)
-    
-""" def next_page_url(current_page):
-    global more_articles_url
-    more_articles_url = []
-    for uri in current_page:
-        uri = current_page + uri
-        more_articles_url.apprend(uri)   """ 
-#^^ how to get the current page url and add 1 to the page=# section to trigger the next page search??
 
-article_url()
+
+
 next_page()
 
 
